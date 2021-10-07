3483c68d2513c185766b9dd418d9b6fa7f6fbf0e
Cesar Acual <agustoacual@gmail.com>
2021-10-06 23:32:09 -0600

    Agregar argumentos desde consola

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const { crearArchivo } = require("./helpers/multiplicar");
 
 console.clear();
 
-const base = 4;
+const [ , , arg3 = 'base=5'] = process.argv
+const [ , base = 5 ] = arg3.split('=')
+
+// const base = 4;
 
 crearArchivo(base)
 	.then( nombreArchivo => console.log(nombreArchivo, 'creado') )
