56572f9c683f54bc5b98e59875b8ff7595d9bf93
Cesar Acual <agustoacual@gmail.com>
2021-11-02 14:49:18 -0600

    Instalar yargs

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+const argv = require('yargs').argv
 
 console.clear();
 
-const [ , , arg3 = 'base=5'] = process.argv
-const [ , base = 5 ] = arg3.split('=')
+console.log(process.argv)
+console.log(argv)
+
+console.log('base: yargs', argv.base)
 
 // const base = 4;
 
-crearArchivo(base)
-	.then( nombreArchivo => console.log(nombreArchivo, 'creado') )
-	.catch( err => console.log(err) );
+// crearArchivo(base)
+// 	.then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+// 	.catch( err => console.log(err) );
