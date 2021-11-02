6fc37814bf0047cff996fa9fe36c0917671afdf3
Cesar Acual <agustoacual@gmail.com>
2021-11-02 15:50:49 -0600

    Usare el paquete de colores

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { crearArchivo } = require("./helpers/multiplicar");
+require("colors")
 const argv = require('./config/yargs')
 
 console.clear();
 
 crearArchivo(argv.b, argv.listar)
-	.then( nombreArchivo => console.log(nombreArchivo, 'creado') )
+	.then( nombreArchivo => console.log(nombreArchivo.rainbow, 'creado') )
 	.catch( err => console.log(err) );
--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,16 +1,17 @@
 const fs = require("fs");
+const colors = require("colors")
 
 const crearArchivo = async (base = 5, listar) => {
   let salida = "";
 
   for (let i = 1; i <= 10; i++) {
-    salida += `${base} * ${i} = ${i * base}\n`;
+    salida += `${base} ${'*'.green} ${i} ${'='.green} ${i * base}\n`;
   }
 
   if (listar) {
-    console.log("=================================");
-    console.log("           Tabla del: ", base);
-    console.log("=================================");
+    console.log("=================================".green);
+    console.log("           Tabla del: ".green, colors.yellow(base));
+    console.log("=================================".green);
     console.log(salida);
   }
     
